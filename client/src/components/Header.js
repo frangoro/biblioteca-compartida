79bb818debe89e9fe59c90248e6215730126b337
Fran Gómez <frangoro@gmail.com>
2025-07-16 18:48:59 +0200

    User widget y correcciones en estilos

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Navigation from './Navigation'; 
 import logo from '../assets/images/logo.png';
+import UserWidget from './UserWidget';
+import '../styles/Header.css';
 
 function Header() {
   return (
     <header className="app-header">
       <div className="container">
-        <img src={logo} className="app-logo" alt="logo" />
-        <h1>Biblioteca Compartida</h1>
-        <Navigation />
+        <div className="primera-fila">
+          <div className="logo-titulo">
+            <img src={logo} className="app-logo" alt="logo" />
+            <h1>Biblioteca Compartida</h1>
+          </div>
+          <UserWidget /> 
+        </div>
+        <div className="segunda-fila">
+          <Navigation />
+        </div>
       </div>
     </header>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,12 @@ import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'; // Importa librería Font Awesome
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'; // Importa el icono de usuario que queremos
+
+// Añade los iconos a la librería global de Font Awesome
+// Esto los hace disponibles para cualquier componente <FontAwesomeIcon>
+library.add(faUserCircle);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
