15cc2c04d2ab6a1d8c83a747d2dd7878532394b8
Fran Gómez <frangoro@gmail.com>
2026-01-04 15:03:11 +0100

    Activo envío de mensajes de contacto

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -3,9 +3,21 @@ const nodemailer = require('nodemailer');
 // Configuramos el transportador
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // Use SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  }
+});
+
+// Para depurar
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("Error en la configuración del correo:", error);
+  } else {
+    console.log("El servidor está listo para enviar correos");
   }
 });
 
